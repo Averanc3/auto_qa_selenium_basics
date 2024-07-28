c68da6cc6412f2daf44e01645a3aa701bef9160e
nikitaf <xxdariusxx@yandex.ru>
2024-07-28 21:35:15 +0300

    added content that somehow wasn't added in previous commit

---
+++ conftest.py
@@ -0,0 +1,54 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options as FFOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", default="ch", choices=["ch", "ff", "eg"]
+    )
+    parser.addoption(
+        "--headless", action="store_true"
+    )
+    parser.addoption(
+        "--url", default='http://192.168.0.63:8080/'
+    )
+
+@pytest.fixture()
+def base_url(request):
+    url = request.config.getoption("--url")
+    return url
+@pytest.fixture()
+def browser(request):
+    browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+
+    driver = None
+
+    if browser_name == "ch":
+        options = ChromeOptions()
+        if headless:
+            options.add_argument("--headless=new")
+        driver = webdriver.Chrome(service=Service(), options=options)
+
+    elif browser_name == "ff":
+        options = FFOptions()
+        if headless:
+            options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+
+    elif browser_name == "eg":
+        options = ChromeOptions()
+        if headless:
+            options.add_argument("headless=new")
+        driver = webdriver.Edge(options=options)
+
+    driver.set_window_size(1920, 1080)
+
+
+
+    yield driver
+
+    driver.quit()
